c8f774d97eeda84f2b511db372e08036e8cc5020
yenlinh000 <yenlinh.nc21@gmail.com>
2021-12-29 11:00:09 +0700

    Add files via upload

---
+++ Login/src/java/Model/Constants.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author A
+ */
+public class Constants {
+    public static String GOOGLE_CLIENT_ID = "955512294158-rksaj6lpbhrq41ui7decvr5tuqu01qhb.apps.googleusercontent.com";
+
+    public static String GOOGLE_CLIENT_SECRET = "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";
+
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/LoginGoogle/LoginGoogleController";
+
+    public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+
+    public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+
+    public static String GOOGLE_GRANT_TYPE = "authorization_code";
+}
